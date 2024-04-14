d485aad916641c0a3a0228b00a3fb14f4108386f
Владислав Белов <belov_vlad@list.ru>
2024-04-14 14:38:19 +0300

    init

---
+++ File_Reference/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace File_Reference
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VaultsLabel = new System.Windows.Forms.Label();
+            this.VaultsComboBox = new System.Windows.Forms.ComboBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.CustRefListBox = new System.Windows.Forms.ListBox();
+            this.AddCustomFileReference = new System.Windows.Forms.Button();
+            this.CustRefOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // VaultsLabel
+            // 
+            this.VaultsLabel.AutoSize = true;
+            this.VaultsLabel.Location = new System.Drawing.Point(13, 26);
+            this.VaultsLabel.Name = "VaultsLabel";
+            this.VaultsLabel.Size = new System.Drawing.Size(244, 13);
+            this.VaultsLabel.TabIndex = 0;
+            this.VaultsLabel.Text = "Copy a file to the clipboard, then select vault view:";
+            // 
+            // VaultsComboBox
+            // 
+            this.VaultsComboBox.FormattingEnabled = true;
+            this.VaultsComboBox.Location = new System.Drawing.Point(16, 59);
+            this.VaultsComboBox.Name = "VaultsComboBox";
+            this.VaultsComboBox.Size = new System.Drawing.Size(121, 21);
+            this.VaultsComboBox.TabIndex = 1;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(16, 98);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(271, 23);
+            this.BrowseButton.TabIndex = 3;
+            this.BrowseButton.Text = "Browse for file to which to add a custom reference...";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // CustRefListBox
+            // 
+            this.CustRefListBox.FormattingEnabled = true;
+            this.CustRefListBox.Location = new System.Drawing.Point(16, 150);
+            this.CustRefListBox.Name = "CustRefListBox";
+            this.CustRefListBox.Size = new System.Drawing.Size(259, 95);
+            this.CustRefListBox.TabIndex = 4;
+            // 
+            // AddCustomFileReference
+            // 
+            this.AddCustomFileReference.Location = new System.Drawing.Point(16, 273);
+            this.AddCustomFileReference.Name = "AddCustomFileReference";
+            this.AddCustomFileReference.Size = new System.Drawing.Size(259, 23);
+            this.AddCustomFileReference.TabIndex = 5;
+            this.AddCustomFileReference.Text = "Add custom file reference";
+            this.AddCustomFileReference.UseVisualStyleBackColor = true;
+            this.AddCustomFileReference.Click += new System.EventHandler(this.AddCustomFileReference_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 308);
+            this.Controls.Add(this.AddCustomFileReference);
+            this.Controls.Add(this.CustRefListBox);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.VaultsComboBox);
+            this.Controls.Add(this.VaultsLabel);
+            this.Name = "Form1";
+            this.Text = "Add custom file references";
+            this.Load += new System.EventHandler(this.FileReferencesCSharp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label VaultsLabel;
+        private System.Windows.Forms.ComboBox VaultsComboBox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.ListBox CustRefListBox;
+        private System.Windows.Forms.Button AddCustomFileReference;
+        private System.Windows.Forms.OpenFileDialog CustRefOpenFileDialog;
+    }
+}
+
---
+++ File_Reference/Form1.cs
@@ -0,0 +1,154 @@
+﻿using EPDM.Interop.epdm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Reference
+{
+  public partial class Form1 : Form
+  {
+        public Form1()
+    {
+        InitializeComponent();
+    }
+
+    IEdmVault5 vault1 = null;
+
+    public void FileReferencesCSharp_Load(System.Object sender, System.EventArgs e)
+    {
+
+        try
+        {
+            IEdmVault5 vault1 = new EdmVault5();
+            IEdmVault8 vault = (IEdmVault8)vault1;
+            EdmViewInfo[] Views = null;
+
+            vault.GetVaultViews(out Views, false);
+            VaultsComboBox.Items.Clear();
+            foreach (EdmViewInfo View in Views)
+            {
+                VaultsComboBox.Items.Add(View.mbsVaultName);
+            }
+            if (VaultsComboBox.Items.Count > 0)
+            {
+                VaultsComboBox.Text = (string)VaultsComboBox.Items[0];
+            }
+        }
+        catch (System.Runtime.InteropServices.COMException ex)
+        {
+            MessageBox.Show("HRESULT = 0x" + ex.ErrorCode.ToString("X") + "\n" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    public void BrowseButton_Click(System.Object sender, System.EventArgs e)
+    {
+        try
+        {
+            CustRefListBox.Items.Clear();
+
+            //Only create a new vault object
+            //if one hasn't been created yet
+            if (vault1 == null)
+                vault1 = new EdmVault5();
+            if (!vault1.IsLoggedIn)
+            {
+                //Log into selected vault as the current user
+                vault1.LoginAuto(VaultsComboBox.Text, this.Handle.ToInt32());
+            }
+
+            //Set the initial directory in the File Open dialog
+            CustRefOpenFileDialog.InitialDirectory = vault1.RootFolderPath;
+            //Show the File Open dialog
+            System.Windows.Forms.DialogResult DialogResult = default(System.Windows.Forms.DialogResult);
+            DialogResult = CustRefOpenFileDialog.ShowDialog();
+            //If the user didn't click Open, exit the sub
+            if (!(DialogResult == System.Windows.Forms.DialogResult.OK))
+                return;
+
+             foreach (string FileName in CustRefOpenFileDialog.FileNames)
+            {
+                CustRefListBox.Items.Add(FileName);
+            }
+        }
+        catch (System.Runtime.InteropServices.COMException ex)
+        {
+            MessageBox.Show("HRESULT = 0x" + ex.ErrorCode.ToString("X") + "\n" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+
+    public void AddCustomFileReference_Click(System.Object sender, System.EventArgs e)
+    {
+        try
+        {
+            //Only create a new vault object
+            //if one hasn't been created yet
+            IEdmVault7 vault2 = null;
+            if (vault1 == null)
+                vault1 = new EdmVault5();
+            vault2 = (IEdmVault7)vault1;
+            if (!vault1.IsLoggedIn)
+            {
+                //Log into selected vault as the current user
+                vault1.LoginAuto(VaultsComboBox.Text, this.Handle.ToInt32());
+            }
+
+            IEdmAddCustomRefs addCustRefs = (IEdmAddCustomRefs)vault2.CreateUtility(EdmUtility.EdmUtil_AddCustomRefs);
+            Int32[] ppoFileIdArray = new Int32[CustRefListBox.Items.Count];
+            IEdmFile5 file = null;
+            IEdmFolder5 parentFolder = null;
+            int i = 0;
+            foreach (string FileName in CustRefListBox.Items)
+            {
+                file = vault2.GetFileFromPath(FileName, out parentFolder);
+                if (!file.IsLocked)
+                {
+                    file.LockFile(parentFolder.ID, this.Handle.ToInt32(), (int)EdmLockFlag.EdmLock_Simple);
+                }
+                ppoFileIdArray[i] = file.ID;
+                i++;
+            }
+            Boolean retCode = false;
+
+            //Add the file that is copied to the clipboard as a custom reference to the selected file
+            foreach (int ID in ppoFileIdArray)
+            {
+                addCustRefs.AddReferencesClipboard(ID);
+                addCustRefs.CreateTree((int)EdmCreateReferenceFlags.Ecrf_Nothing);
+                addCustRefs.ShowDlg(this.Handle.ToInt32());
+                retCode = addCustRefs.CreateReferences();
+            }
+
+            // Check in the file
+            file.UnlockFile(this.Handle.ToInt32(), "Custom reference added");
+
+            //Display current custom file references
+            retCode = addCustRefs.ShowEditReferencesDlg(ref ppoFileIdArray, this.Handle.ToInt32());
+
+        }
+        catch (System.Runtime.InteropServices.COMException ex)
+        {
+            MessageBox.Show("HRESULT = 0x" + ex.ErrorCode.ToString("X") + "\n" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+}
+}
